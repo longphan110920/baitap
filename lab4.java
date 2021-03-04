71daccd82f09815c0f61974ce0600864ea35cb2b
longphan110920 <78297881+longphan110920@users.noreply.github.com>
2021-03-04 18:32:22 +0700

    Add files via upload

---
+++ lab4.java
@@ -0,0 +1,26 @@
+package bt;
+
+public class lab4 {
+	public static void main(String[] args) {
+		//bai 1
+		SanPham sp1 = new SanPham();
+		sp1.ten="San Pham 1";//gan ten cho sp1
+		sp1.gia=123;//gan gia cho sp1
+		sp1.giamGia=11;
+		sp1.xuat();
+		
+		// bai 2
+//		SanPham sp1 = new SanPham();
+//	    SanPham sp2 = new SanPham();
+//		sp1.nhap();
+//		sp1.xuat();
+//		sp2.nhap();
+//		sp2.xuat();
+		
+		//// bai 3
+//		SanPham sp1 = new SanPham("Iphone", 28, 2);
+//		SanPham sp2 = new SanPham("Ipad", 15);
+//		sp1.xuat();
+//		sp2.xuat();
+	}
+}
